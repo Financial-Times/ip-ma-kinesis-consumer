074aabcc3a1d8d1a89e15c077a58ae48e8e44736
Andrew <andrewcsnead@gmail.com>
2017-09-30 22:15:03 +0100

    temp logs

--- bin/render-properties.js
+++ bin/render-properties.js
@@ -7,12 +7,11 @@ const handlebars = require('handlebars');
 const config = require('../config');
 
 let templ;
-console.log(config.streams);
 for (const stream of Object.keys(config.streams)) {
   try {
     templ = fs.readFileSync(process.argv[2], 'utf8');
     const output = handlebars.compile(templ)(config.streams[stream]);
-    console.log(stream);
+    console.log(`./app/${config.streams[stream].streamName}.properties`);
     fs.writeFileSync(`./app/${config.streams[stream].streamName}.properties`, output, 'utf-8');
   } catch (err) {
     console.error(err);
