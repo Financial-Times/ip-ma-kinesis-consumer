70cb4e1e73d75bf23aa7245dbd21c69b4484abf6
Andrew <andrewcsnead@gmail.com>
2018-01-30 14:44:19 +0000

    add temp check for ft user on email events

--- app/records/recordFilter.js
+++ app/records/recordFilter.js
@@ -3,12 +3,14 @@ const selectn = require('selectn');
 const messageType = selectn('ingest.context.messageType');
 const action = selectn('ingest.action');
 const isStaff = selectn('user.subscriptions.staff');
+const user = selectn('user.uuid');
 
 module.exports = () => {
   return (record = {}) => {
     if (messageType(record) === 'UserProductsChanged'
       || messageType(record) === 'SubscriptionPurchased'
-      || (messageType(record) === 'EmailEvent' && action(record) === 'click')) {
+      || (messageType(record) === 'EmailEvent' && action(record) === 'click')
+      || (messageType(record) === 'EmailEvent' && user(record) === 'a73e0b60-e669-40b3-ba5e-0c8621cadac1')) {
       return true;
     }
     return false;
