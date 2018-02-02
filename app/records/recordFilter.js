967ed5381b57d86cb21188a997dde4cf00bed4ba
Andrew <andrewcsnead@gmail.com>
2018-02-02 09:00:22 +0000

    add emaiId for whitelist

--- app/records/recordFilter.js
+++ app/records/recordFilter.js
@@ -2,6 +2,7 @@ const selectn = require('selectn');
 
 const messageType = selectn('ingest.context.messageType');
 const action = selectn('ingest.action');
+const emailId = selectn('ingest.context.parentEmailId');
 const isStaff = selectn('user.subscriptions.staff');
 const user = selectn('user.uuid');
 
@@ -12,7 +13,7 @@ module.exports = () => {
       || messageType(record) === 'SubscriptionPaymentFailure'
       || messageType(record) === 'SubscriptionPaymentSuccess'
       || (messageType(record) === 'EmailEvent' && action(record) === 'click')
-      || (messageType(record) === 'EmailEvent' && user(record) === 'a73e0b60-e669-40b3-ba5e-0c8621cadac1')) {
+      || (messageType(record) === 'EmailEvent' && action(record) === 'injection' && emailId(record) === '584010ed69bff20400ec3dd6')) {
       return true;
     }
     return false;
