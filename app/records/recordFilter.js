4d2a6ffd40350cbb76e71dbcac0e618f30a92de3
Andrew <andrewcsnead@gmail.com>
2018-02-16 08:00:09 +0000

    add SubscriptionCancelRequestProcessed

--- app/records/recordFilter.js
+++ app/records/recordFilter.js
@@ -12,6 +12,7 @@ module.exports = () => {
       || messageType(record) === 'SubscriptionPurchased'
       || messageType(record) === 'SubscriptionPaymentFailure'
       || messageType(record) === 'SubscriptionPaymentSuccess'
+      || messageType(record) === 'SubscriptionCancelRequestProcessed'
       || (messageType(record) === 'EmailEvent' && action(record) === 'click')
       || (messageType(record) === 'EmailEvent' && action(record) === 'injection' && emailId(record) === '584010ed69bff20400ec3dd6')) {
       return true;
